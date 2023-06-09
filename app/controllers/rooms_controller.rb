9a4e9d12871366e7717087c914175c2c9311157f
81168Tli <135498013+81168Tli@users.noreply.github.com>
2023-06-09 19:25:06 +0900

    チャットルームの削除機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,7 +5,6 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
     
-    
   end
 
   def create
@@ -15,6 +14,12 @@ class RoomsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
   end
 
   private
